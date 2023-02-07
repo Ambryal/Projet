369ed3e713f7e9ff645bcc90130d4ef30d27be63
LouisonRbo <louison.rbo@gmail.com>
2023-02-07 10:21:35 +0100

    commit

--- commit.bat
+++
@@ -1,5 +0,0 @@
-git config --global user.email "gohieralexis@yahoo.fr"
-git config --global user.name "Ambryal"
-git add .
-git commit -a -m commit
-git push
---
+++ commit.sh
@@ -0,0 +1,5 @@
+git config --global user.email "louison.rbo@gmail.com"
+git config --global user.name "LouisonRbo"
+git add .
+git commit -a -m commit
+git push
