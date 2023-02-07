0c503b2f46be233f876f61e613ab2de4cef4fcc6
Ambryal <gohieralexis@yahoo.fr>
2023-02-07 10:23:35 +0100

    commit

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
-git config --global user.email "gohieralexis@gmail.com"
+git config --global user.email "gohieralexis@yahoo.fr"
 git config --global user.name "Ambryal"
 git add .
 git commit -a -m commit
