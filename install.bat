7a21e3e7a72a8cd6165582c182a96c0a3d5830c6
Ambryal <85936037+Ambryal@users.noreply.github.com>
2023-02-10 12:35:40 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,8 @@
 git clone https://github.com/Ambryal/Projet.git
 cd Projet
 python -m venv venv
-venv\Scripts\pip install pdfminer
+venv/bin/pip install pdfminer-six
+venv/bin/pip install pypdf2
+venv/bin/pip install aspose-words
+venv/bin/pip install pypdf
+venv/bin/pip install pypdfium2
