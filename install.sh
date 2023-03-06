7b27fa7f8ae3705595bf5c143724a2e39b15c487
Ambryal <gohieralexis@yahoo.fr>
2023-03-06 15:52:58 +0100

    commit

--- install.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/Ambryal/Projet.git
-cd Projet
-python -m venv venv
-venv/bin/pip install pdfminer-six
-venv/bin/pip install pypdf2
-venv/bin/pip install aspose-words
-venv/bin/pip install pypdf
-venv/bin/pip install pypdfium2
